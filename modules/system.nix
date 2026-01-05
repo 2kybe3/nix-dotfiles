46ca1d698d94572995ddcda35c46987d24d8750f
2kybe3 <2kybe3>
2026-01-05 06:05:35 +0100

    mullvad

--- modules/system.nix
+++ modules/system.nix
@@ -3,11 +3,14 @@
 {
   networking.hostName = "knx";
   networking.networkmanager.enable = true;
+  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
 
   time.timeZone = "Europe/Berlin";
 
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "us";
+
+  hardware.bluetooth.enable = true;
 
   ##### Users #####
   users.users.kybe = {
@@ -29,13 +32,20 @@
   ##### Services #####
 
   services.printing.enable = true;
+  services.mullvad-vpn.enable = true;
 
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
 
-  hardware.bluetooth.enable = true;
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    dnsovertls = "true";
+  };
 
   system.stateVersion = "25.11";
 }
