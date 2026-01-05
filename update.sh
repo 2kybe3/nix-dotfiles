fa9fe510ae09c9889dd89808abd19f51052c80f6
2kybe3 <2kybe3>
2026-01-05 19:58:44 +0100

    Config-change 2026-01-05 19:58:44

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+COMMIT_MSG="Config-change $(date '+%Y-%m-%d %H:%M:%S')"
+
+git add -A
+
+if git diff-index --quit HEAD --; then
+	echo "No changes to commit."
+else
+	 git commit -m "$COMMIT_MSG"
+	 echo "Committed changes with message: '$COMMIT_MSG'"
+fi
+
+sudo nixos-rebuild switch --flake /home/kybe/.dotfiles#knx
+
+echo "NixOS flake reloaded!"
