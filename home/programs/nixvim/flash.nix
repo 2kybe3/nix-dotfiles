86c954f87d1f94fec03627a32c50541d217fb766
2kybe3 <kybe@kybe.xyz>
2026-01-19 20:27:12 +0100

    tweak flash binds

--- home/programs/nixvim/flash.nix
+++ home/programs/nixvim/flash.nix
@@ -3,8 +3,7 @@
     enable = true;
     luaConfig.post = ''
       vim.keymap.set({ "n", "x", "o" }, "zk", function() require("flash").jump() end, { desc = "flash" })
-      vim.keymap.set({ "n", "x", "o" }, "Zk", function() require("flash").treesitter() end, { desc = "flash treesitter" })
-      vim.keymap.set("c", "<c-s>", function() require("flash").toggle() end, { desc = "toggle flash search" })
+      vim.keymap.set("c", "<C-s>", function() require("flash").toggle() end, { desc = "toggle flash search" })
     '';
   };
 }
