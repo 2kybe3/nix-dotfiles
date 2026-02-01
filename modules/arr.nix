7c9aae568189ea5bf70c6b0dfd5d0c0acbacc224
2kybe3 <kybe@kybe.xyz>
2026-02-01 20:14:26 +0100

    add jellyfin

--- modules/arr.nix
+++ modules/arr.nix
@@ -6,25 +6,34 @@ let
 in
 {
   services = {
+    jellyfin.enable = true;
+
     sonarr.enable = true;
     radarr.enable = true;
     prowlarr.enable = true;
-    jackett.enable = true;
+
     bazarr.enable = true;
+    jackett.enable = true;
+
     flaresolverr.enable = true;
+
     readarr = {
       enable = false; # TODO
       user = if config.services.calibre-server.enable then config.calibre-server.user else "readarr";
     };
-
     
     caddy.virtualHosts = {
+      "jellyfin.${domain}" = createCaddyProxy 5055;
+
       "sonarr.${domain}" = createCaddyProxy 8989;
       "radarr.${domain}" = createCaddyProxy 7878;
       "prowlarr.${domain}" = createCaddyProxy 9696;
-      "jackett.${domain}" = createCaddyProxy 9117;
+
       "bazarr.${domain}" = createCaddyProxy 6767;
+      "jackett.${domain}" = createCaddyProxy 9117;
+
       "flaresolverr.${domain}" = createCaddyProxy 8191;
+
       "readarr.${domain}" = createCaddyProxy 8787;
     };
   };
