3f2c8c92ce80e64b6183c134710e29814a7d2ce2
2kybe3 <kybe@kybe.xyz>
2026-02-07 18:05:23 +0100

    add auto optimize to nix

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,9 +1,12 @@
 {
   nix = {
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+          "flakes"
+      ];
+    };
     gc = {
       automatic = true;
       dates = "20:00";
