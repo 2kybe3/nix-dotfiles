add502b9d2c31cbc859c47a2dc1e125336df8ba9
2kybe3 <kybe@kybe.xyz>
2026-02-09 18:37:11 +0100

    improve syncthing id generation

--- tools/syncthing-folder-id-gen/script.sh
+++ tools/syncthing-folder-id-gen/script.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-openssl rand 10 \
-| base32 \
-| tr 'A-Z2-7' 'a-z2-7' \
-| cut -c1-10 \
-| sed 's/.\{5\}/&-/'
+
+n=${1:-1}
+for ((i=0;i<n;i++)); do
+    openssl rand 10 \
+        | base32 \
+        | tr 'A-Z2-7' 'a-z2-7' \
+        | cut -c1-10 \
+        | sed 's/.\{5\}/&-/'
+done
