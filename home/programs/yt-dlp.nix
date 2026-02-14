5a8a412c9e66227b91ab00e68932252ed72b9e7b
2kybe3 <kybe@kybe.xyz>
2026-02-14 23:22:56 +0100

    yt-dlp output flag

--- home/programs/yt-dlp.nix
+++ home/programs/yt-dlp.nix
@@ -4,6 +4,7 @@
     settings = {
       downloader = "aria2c";
       downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
+      output = "~/YouTube/%(title)s.%(ext)s";
     };
   };
   home.packages = with pkgs; [aria2];
