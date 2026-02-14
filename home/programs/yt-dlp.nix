2664b27d495d8d876d5afe282ca4ac9deb8a751e
2kybe3 <kybe@kybe.xyz>
2026-02-15 00:38:43 +0100

    yt-dlp user-agent and cookies from browser for auth

--- home/programs/yt-dlp.nix
+++ home/programs/yt-dlp.nix
@@ -1,4 +1,6 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  firefoxVersion = pkgs.lib.concatStringsSep "." (pkgs.lib.take 2 (pkgs.lib.splitString "." pkgs.firefox.version));
+in {
   programs.yt-dlp = {
     enable = true;
     settings = {
@@ -6,6 +8,8 @@
       remux-video = "mp4";
       downloader-args = "aria2c:'-c -x16 -s16 -k4M --file-allocation=falloc'";
       output = "~/YouTube/%(title)s.%(ext)s";
+      user-agent = "Mozilla/5.0 (X11; Linux x86_64; rv:${firefoxVersion}) Gecko/20100101 Firefox/${firefoxVersion}";
+      cookies-from-browser = "firefox";
     };
   };
   home.packages = with pkgs; [aria2];
