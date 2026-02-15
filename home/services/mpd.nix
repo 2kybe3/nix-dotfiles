4132beb32493715364c5b797132013c55d93910e
2kybe3 <kybe@kybe.xyz>
2026-02-15 22:07:15 +0100

    mpd-mpris

--- home/services/mpd.nix
+++ home/services/mpd.nix
@@ -1,13 +1,17 @@
-{config, ...}: {
-  services.mpd = {
-    enable = true;
-    extraArgs = ["--verbose"];
-    network.startWhenNeeded = true;
-    extraConfig = ''
-      audio_output {
-        type "pulse"
-        name "PulseAudio"
-      }
-    '';
+{pkgs, ...}: {
+  services = {
+    mpd = {
+      enable = true;
+      extraArgs = ["--verbose"];
+      network.startWhenNeeded = true;
+      extraConfig = ''
+        audio_output {
+          type "pulse"
+          name "PulseAudio"
+        }
+      '';
+    };
+    mpd-mpris.enable = true;
   };
+  home.packages = [pkgs.playerctl];
 }
