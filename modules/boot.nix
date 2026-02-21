711b0d67f4f1ba74ea5ba082b78eee1749321159
2kybe3 <kybe@kybe.xyz>
2026-02-21 20:43:42 +0100

    tmp cleaning

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,10 +1,15 @@
-{
-  boot.loader = {
-    systemd-boot = {
-      enable = true;
-      memtest86.enable = true;
-    };
+{config, ...}: {
+  boot = {
+    stage2Greeting = "<<< ${config.kybe.lib.hostName}: ${config.system.nixos.distroName} Stage 2 :-) >>>";
+    tmp.cleanOnBoot = true;
 
-    efi.canTouchEfiVariables = true;
+    loader = {
+      systemd-boot = {
+        enable = true;
+        memtest86.enable = true;
+      };
+
+      efi.canTouchEfiVariables = true;
+    };
   };
 }
