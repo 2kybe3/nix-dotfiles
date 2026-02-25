d8419c5d7768321e4d5e0f7d88b921dfc3ee28db
2kybe3 <kybe@kybe.xyz>
2026-02-25 19:49:36 +0100

    tweaks

--- containers/i2pd.nix
+++ containers/i2pd.nix
@@ -1,24 +1,26 @@
-{
+{self, ...}: {
   containers.i2pd = {
     autoStart = true;
     ephemeral = true;
 
-    config = {...}: {
-      networking.firewall.allowedTCPPorts = [
-        7070
-        4444
-      ];
+    config = {...}:
+      import "${self}/containers/shared.nix"
+      // {
+        networking.firewall.allowedTCPPorts = [
+          7070
+          4444
+        ];
 
-      services.i2pd = {
-        enable = true;
-        address = "0.0.0.0";
-        proto = {
-          http.enable = true;
-          httpProxy.enable = true;
+        services.i2pd = {
+          enable = true;
+          address = "0.0.0.0";
+          proto = {
+            http.enable = true;
+            httpProxy.enable = true;
+          };
         };
-      };
 
-      system.stateVersion = "25.11";
-    };
+        system.stateVersion = "25.11";
+      };
   };
 }
